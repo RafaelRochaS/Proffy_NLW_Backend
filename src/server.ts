2db4d9232c468efdd09f690ebd38285b160b0cc3
Rafael Souza <rafaelrochasouza@outlook.com.br>
2020-08-07 18:23:36 -0300

    add dotenv

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import routes from './routes';
 import cors from 'cors';
 
+import { PORT } from './utils/configs'
+
 const app = express();
 
 app.use(express.json());
-app.use(routes);
 app.use(cors());
+app.use(routes);
 
-app.listen(3333, '0.0.0.0');
+app.listen(PORT);
